cf3222d2c72c078b905c85d6ccb1016ffc345943
Saqlain Abbas <saqlain572@gmail.com>
2025-06-02 18:05:54 +0500

    Initial commit - Job Board API

---
+++ JobBoardAPI/Program.cs
@@ -0,0 +1,37 @@
+using JobBoardAPI.Data;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.File("Logs/log.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+
+builder.Host.UseSerilog();
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// Log HTTP requests
+app.UseSerilogRequestLogging();
+
+app.UseHttpsRedirection();
+app.MapControllers();
+app.Run();
